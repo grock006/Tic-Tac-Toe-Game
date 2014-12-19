b60180339741b669fe4801c893f168d4d21e8949
Grock006 <grock006@gmail.com>
2014-12-19 11:18:05 -0800

    first commit

--- js/TictactoeController.js
+++ js/TictactoeController.js
@@ -1,74 +1,155 @@
-angular
-	.module('tictactoeApp')
-	.controller('TictactoeController', TictactoeController);
+var app = angular.module('tictactoeApp', ['firebase']);
+	
+	app.controller('TictactoeController', TictactoeController);
 
-	TictactoeController.$inject = ['$firebase']
+	TictactoeController.$inject = ['$firebase'];
 
-	function TodosController($firebase){
+	function TictactoeController($firebase){
 		  var controller = this;
 
-		  controller.className = null;
- 		  controller.playerOne = true;
- 		  controller.playerTwo = false;
- 		  console.log(controller.playerOne);
+		  controller.game = getGameObject();
 
-		 controller.row = [{section:0, className: ""}, 
- 			       {section:1, className: ""}, 
- 				   {section:2, className: ""}, 
- 				   {section:3, className: ""}, 
- 				   {section:4, className: ""}, 
- 				   {section:5, className: ""}, 
- 				   {section:6, className: ""}, 
- 				   {section:7, className: ""}, 
- 				   {section:8, className: ""}];
+		  controller.gameOver = "GAME OVER";
 
+		  controller.newGame = newGame;
+		  controller.joinTeam = joinTeam;
+		  controller.startGame = startGame;
+		  controller.checkScore = checkScore;
+		  controller.changeScore = changeScore;
+		  
+		  controller.team = null;
+		  controller.hansWins = 0;
+		  controller.mcclaneWins = 0;
 
+	  function getGameObject() {
+    var ref = new Firebase("https://diehardtictactoe.firebaseio.com/game");
+    return $firebase(ref).$asObject();
+  }
 
-		     function checkScore(){
+		 function newGame(){
+		 	  controller.row = [{},{},{},{},{},{},{},{},{}];	  
+		 	  controller.status = "";	
+		 	  controller.source = "";
+		 	  controller.team = "";
+		 	  controller.turn = "";
+		 	  controller.choose = "";		   
+		 }
+
+		 function joinTeam(teamPick){
+		 	controller.team = teamPick;
+		 	controller.turn = teamPick;
+		 }
+
+		 function startGame(){
+		 	controller.game.row = [{a:1},{a:1},{a:1},{a:1},{a:1},{a:1},{a:1},{a:1},{a:1}];
+		 	controller.game.status = "";	
+		 	controller.game.source = "";
+		  controller.team = "";
+		  controller.turn = "";
+		  controller.choose = "CHOOSE YOUR TEAM";
+		  controller.game.$save();
+		 }
+
+		 function checkScore(){
 		 	    if(controller.row[0].className == "markerx" && controller.row[1].className == "markerx" && controller.row[2].className == "markerx"){
-		 	     controller.status = "YIPPIE KI YAY, MOTHERFUCKER!!!!!";	
-		 	     controller.source = "images/one_wins.gif";
+		 	    controller.status = "YIPPIE KI YAY, MOTHERFUCKER!!!!!";	
+		 	    controller.source = "images/one_wins.gif";
+		 	    controller.mcclaneWins++;
+		 	    }
+		 	    else if(controller.row[3].className == "markerx" && controller.row[4].className == "markerx" && controller.row[5].className == "markerx"){
+		 	    controller.status = "YIPPIE KI YAY, MOTHERFUCKER!!!!!";	
+		 	    controller.source = "images/one_wins.gif";
+		 	     controller.mcclaneWins++;
+		 	    }
+		 	    else if(controller.row[6].className == "markerx" && controller.row[7].className == "markerx" && controller.row[8].className == "markerx"){
+		 	    controller.status = "YIPPIE KI YAY, MOTHERFUCKER!!!!!";	
+		 	    controller.source = "images/one_wins.gif";
+		 	     controller.mcclaneWins++;
 		 	    }
 		 	    else if(controller.row[0].className == "markerx" && controller.row[3].className == "markerx" && controller.row[6].className == "markerx"){
 		 	    controller.status = "WELCOME TO THE PARTY, PAL!";
 		 	    controller.source = "images/one_wins2.gif";	
+		 	     controller.mcclaneWins++;
+		 	    }
+		 	    else if(controller.row[1].className == "markerx" && controller.row[4].className == "markerx" && controller.row[7].className == "markerx"){
+		 	    controller.status = "WELCOME TO THE PARTY, PAL!";
+		 	    controller.source = "images/one_wins2.gif";	
+		 	     controller.mcclaneWins++;
+		 	    }
+		 	    else if(controller.row[2].className == "markerx" && controller.row[5].className == "markerx" && controller.row[8].className == "markerx"){
+		 	    controller.status = "WELCOME TO THE PARTY, PAL!";
+		 	    controller.source = "images/one_wins2.gif";	
+		 	     controller.mcclaneWins++;
 		 	    }
 		 	    else if(controller.row[0].className == "markerx" && controller.row[4].className == "markerx" && controller.row[8].className == "markerx"){
 		 	    controller.status = "Just a fly in the ointment, Hans. The monkey in the wrench. The pain in the ass.";	
 		 	    controller.source = "images/one_wins4.gif";
+		 	     controller.mcclaneWins++;
+		 	    }
+		 	    else if(controller.row[2].className == "markerx" && controller.row[4].className == "markerx" && controller.row[6].className == "markerx"){
+		 	    controller.status = "Just a fly in the ointment, Hans. The monkey in the wrench. The pain in the ass.";	
+		 	    controller.source = "images/one_wins4.gif";
+		 	     controller.mcclaneWins++;
 		 	    }
 		 	    else if(controller.row[0].className == "markero" && controller.row[1].className == "markero" && controller.row[2].className == "markero"){
 		 	    controller.status = "Just another American who saw too many movies as a child? Another orphan of a bankrupt culture who thinks he's John Wayne? Rambo? Marshal Dillon?";	
 		 	    controller.source = "images/two_wins.gif";
+		 	    controller.hansWins++;
+		 	    }
+		 	    else if(controller.row[3].className == "markero" && controller.row[4].className == "markero" && controller.row[5].className == "markero"){
+		 	    controller.status = "Just another American who saw too many movies as a child? Another orphan of a bankrupt culture who thinks he's John Wayne? Rambo? Marshal Dillon?";	
+		 	    controller.source = "images/two_wins.gif";
+		 	    controller.hansWins++;
+		 	    }
+		 	    else if(controller.row[6].className == "markero" && controller.row[7].className == "markero" && controller.row[8].className == "markero"){
+		 	    controller.status = "Just another American who saw too many movies as a child? Another orphan of a bankrupt culture who thinks he's John Wayne? Rambo? Marshal Dillon?";	
+		 	    controller.source = "images/two_wins.gif";
+		 	    controller.hansWins++;
 		 	    }
 		 	    else if(controller.row[0].className == "markero" && controller.row[3].className == "markero" && controller.row[6].className == "markero"){
 		 	    controller.status = "'And when Alexander saw the breadth of his domain, he wept, for there were no more worlds to conquer.' Benefits of a classical education.";
 		 	    controller.source = "images/two_wins2.gif";	
+		 	    controller.hansWins++;
+		 	    }
+		 	    else if(controller.row[1].className == "markero" && controller.row[4].className == "markero" && controller.row[7].className == "markero"){
+		 	    controller.status = "'And when Alexander saw the breadth of his domain, he wept, for there were no more worlds to conquer.' Benefits of a classical education.";
+		 	    controller.source = "images/two_wins2.gif";	
+		 	    controller.hansWins++;
+		 	    }
+		 	    else if(controller.row[2].className == "markero" && controller.row[5].className == "markero" && controller.row[8].className == "markero"){
+		 	    controller.status = "'And when Alexander saw the breadth of his domain, he wept, for there were no more worlds to conquer.' Benefits of a classical education.";
+		 	    controller.source = "images/two_wins2.gif";	
+		 	    controller.hansWins++;
 		 	    }
 		 	    else if(controller.row[0].className == "markero" && controller.row[4].className == "markero" && controller.row[8].className == "markero"){
 		 	    controller.status = "This time John Wayne does not walk off into the sunset with Grace Kelly.";	
 		 	    controller.source = "images/two_wins3.gif";
+		 	    controller.hansWins++;
+		 	    }
+		 	    else if(controller.row[2].className == "markero" && controller.row[4].className == "markero" && controller.row[6].className == "markero"){
+		 	    controller.status = "This time John Wayne does not walk off into the sunset with Grace Kelly.";	
+		 	    controller.source = "images/two_wins3.gif";
+		 	    controller.hansWins++;
 		 	    }
 		 	};
 
-				//change this to $index in the view
-			    function changeClass(row){
-			 		if(controller.playerOne == true){
-			 		row.className = "markerx";
-			 		controller.playerOne = false;
-			 		console.log(controller.playerOne);
-			 		//alert not your turn
-			 		}
-			 		else {
-			 		   row.className = "markero";
-			 		     controller.playerOne = true;
-			 		}
-			 		//set playerOne to false, check it in console.log
-			 		// if playerOne is false then markero
-					  controller.checkScore();
+		 	function changeScore(row){
+		 		if(controller.team == "McClane" || controller.team == "Gruber"){
+		 			if(controller.team == controller.turn){
+		 				if(row.className != "markerx" || row.className != "markero"){
+		 					if(controller.team == "McClane"){
+		 						row.className = "markerx";
+		 		    		controller.turn = "Gruber";
+		 					}
+		 					else if(controller.team == "Gruber") {
+		 		   			row.className = "markero";
+		 		   			controller.turn = "McClane";		
+		 					} 
+		 				}
+			  	}
+				}
+				controller.checkScore();
+			};
 
-				};
 
-
-
-	};
+};
